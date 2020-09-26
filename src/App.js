046502424af4dedb77c8374f1e44a10bac7dce36
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-26 11:53:43 +1000

    40. Class Methods and Arrow Functions

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -20,6 +22,9 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
+  }
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -30,7 +35,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
