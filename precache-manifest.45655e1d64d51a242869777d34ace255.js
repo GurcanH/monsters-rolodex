2270d90de6fc19ba116fd7631a1569298e811af1
Gurcan Hamali <gurcanhamali@gmail.com>
2020-09-26 12:14:43 +1000

    Updates

---
+++ precache-manifest.45655e1d64d51a242869777d34ace255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7cb66ad45176696213334f47a8304f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a0a4c4312e0a91643e4",
+    "url": "/monsters-rolodex/static/css/main.b4ebe43e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0a4c4312e0a91643e4",
+    "url": "/monsters-rolodex/static/js/main.209c0dc5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
